488150c29f39f631ce7035bb8b812cb12cae968c
stanley58585858-design <stanley58585858@gmail.com>
2025-09-22 14:55:35 +0800

    Add SLIF PWA kit

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'slif-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './slif_app_event_segments_regions_mapV2_tracks_highlight.html',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+});
+self.addEventListener('fetch', e=>{
+  e.respondWith(caches.match(e.request).then(res=> res || fetch(e.request)));
+});
